92ee9a7fa3ff5cb0dad103b6fb7aff8a0a5bf548
Saif-PC <Saif.Kagad@lntinfotech.com>
2020-12-16 16:36:54 +0530

    CS Prime no

---
+++ PrimeNo/PrimeNo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min, max,flag;
+            Console.WriteLine("Enter min number");
+            min = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter max number");
+            max = int.Parse(Console.ReadLine());
+            for(int i=min+1;i<max;++i)
+            {
+                flag = 0;
+                for(int j=2;j<i/2;++j)
+                {
+                    if(i%j ==0)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0)
+                    Console.WriteLine(i);
+            }
+
+        }
+    }
+}
